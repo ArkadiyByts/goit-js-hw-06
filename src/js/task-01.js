6181631edeb9014ea1b4f5548369f8ccdd4e80ee
Arkadiy Byts <bytsarkadiy@gmail.com>
2022-11-25 17:22:04 +0100

    first dome

---
+++ src/js/task-01.js
@@ -0,0 +1,28 @@
+const root = document;
+const html = root.lastChild;
+const body = html.lastChild;
+const goBackLink = document.body.children[0];
+const mainList = document.body.children[1];
+const firstItem = mainList.firstElementChild;
+const secondItem = mainList.firstElementChild.nextElementSibling;
+const lastItem = mainList.lastElementChild;
+const textOfSecondItem = firstItem.nextElementSibling.textContent;
+const categoriesCount = mainList.childElementCount;
+const firstCategoryTitle = firstItem.firstElementChild.textContent;
+const secondCategoryTitle = secondItem.firstElementChild.textContent;
+const lastCategoryTitle = lastItem.firstElementChild.textContent;
+const animalsItemsCount = firstItem.lastElementChild.childElementCount;
+const productsItemsCount = secondItem.lastElementChild.childElementCount;
+const technologiesItemsCount = lastItem.lastElementChild.childElementCount;
+
+
+console.log('Number of categories: ' + categoriesCount);
+
+console.log('Category: ' + firstCategoryTitle);
+console.log('Elements: ' + animalsItemsCount);
+
+console.log('Category: ' + secondCategoryTitle);
+console.log('Elements: ' + productsItemsCount);
+
+console.log('Category: ' + lastCategoryTitle);
+console.log('Elements: ' + technologiesItemsCount);
