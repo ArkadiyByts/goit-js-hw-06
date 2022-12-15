02f4ea35b25435446d26294e05026c57f5c00179
Arkadiy Byts <bytsarkadiy@gmail.com>
2022-12-15 13:13:03 +0100

    1:12

---
+++ src/js/task-07.js
@@ -0,0 +1,21 @@
+const range = document.querySelector('#font-size-control');
+console.log(range.value);
+let rangeValue = range.getAttribute('value');
+console.log(rangeValue);
+let span = document.querySelector('#text');
+console.log(span);
+span.st
+
+//range.addEventListener("click", () => {
+    if (range.value > rangeValue) {
+      rangeValue === range.value;  
+    }
+    
+  
+        
+    
+//});
+
+//$("#sli#font-size-controlder").on("input",function () {
+    //        $('#text').css("font-size", $(this).val() + "px");
+  //  });
